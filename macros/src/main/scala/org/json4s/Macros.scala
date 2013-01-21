ef71c397bde6071e2cd1a2322b7d1c5c3e27617c
Bryce Anderson <bryce__anderson@hotmail.com>
2013-01-20 19:33:36 -0500

    Added a elementary ArraySeparator and a matching JsonValueProvider
    Moved a few things around as well.
    Macro deserializer still needs to be adapted to use array access
    patterns, but that can wait until they solidify.

--- macros/src/main/scala/org/json4s/Macros.scala
+++ macros/src/main/scala/org/json4s/Macros.scala
@@ -5,8 +5,9 @@ import language.experimental.macros
 
 object Macros {
   type ParamsTpe = playground.ValueProvider[_]
+  
   def serialize[U](obj:U, name:String, writer: macroimpls.Serializer.Writer) = macro macroimpls.Serializer.impl[U]
   
-  def deserialize[U](params: ParamsTpe,name:String) = macro macroimpls.Deserializer.classbuilder[U]
+  def deserialize[U](params: ParamsTpe,name:String) = macro macroimpls.Deserializer.deserialize_impl[U]
 
 }
