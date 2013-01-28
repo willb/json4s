6c50f0d767c89ae2bd1b69325c6c5796f79ff7c0
Bryce Anderson <bryce.anderson22@gmail.com>
2013-01-28 08:38:42 -0500

    Added support for BigInt and BigDecimal to macrohelpers
    Added unit tests that convert structures to and from Json using macros,
    value providers, and writers.

--- macros/src/main/scala/org/json4s/macroimpls/macrohelpers/PrimativeHelpers.scala
+++ macros/src/main/scala/org/json4s/macroimpls/macrohelpers/PrimativeHelpers.scala
@@ -12,42 +12,48 @@ object PrimativeHelpers {
   def getInt(name:String,in:Any):Int = in match {
     case n:Long => n.asInstanceOf[Int]
     case n:Int => n
+    case bi: math.BigInt => bi.toInt
     case s:String => try { s.toInt } catch {
       case e:java.lang.NumberFormatException => 
-      throw new ParseException(s"Error parsing value '$name' to Int",e)
+      throw new ParseException(s"Error parsing value '$name' to Int.",e)
     }
-    case e => throw new ParseException(s"Error converting item '$name' to Int. Value: $e",null)
+    case e => throw new ParseException(s"Error converting item '$name' to Int. Value: $e, Type: ${e.getClass}",null)
   }
   
   def getLong(name:String,in:Any):Long = in match {
     case n:Long => n
     case n:Int => n.asInstanceOf[Long]
+    case bi: math.BigInt => bi.toLong
     case s:String => try { s.toLong } catch {
       case e:java.lang.NumberFormatException => 
       throw new ParseException(s"Error parsing value '$name' to Long",e)
     }
-    case e => throw new ParseException(s"Error converting item '$name' to Long. Value: $e",null)
+    case e => throw new ParseException(s"Error converting item '$name' to Long. Value: $e, Type: ${e.getClass}",null)
   }
   
   def getFloat(name:String,in:Any):Float = in match {
     case n:Float => n
     case n:Double => n.asInstanceOf[Float]
+    case b:math.BigDecimal => b.toFloat
     case n:Long =>   n.asInstanceOf[Float]
     case n:Int =>    n.asInstanceOf[Float]
     case s:String => s.toFloat
-    case e => throw new ParseException(s"Error converting item '$name' to Float. Value: $e",null)
+    case bi: math.BigInt => bi.toFloat
+    case e => throw new ParseException(s"Error converting item '$name' to Float. Value: $e, Type: ${e.getClass}",null)
   }
   
   def getDouble(name:String,in:Any):Double = in match {
     case n:Double => n
     case n:Float => n.asInstanceOf[Double]
+    case b:math.BigDecimal => b.toDouble
     case n:Long =>   n.asInstanceOf[Double]
     case n:Int =>    n.asInstanceOf[Double]
     case s:String => try { s.toDouble } catch {
       case e:java.lang.NumberFormatException => 
       throw new ParseException(s"Error parsing value '$name' to Double",e)
     }
-    case e => throw new ParseException(s"Error converting item '$name' to Double. Value: $e",null)
+    case bi: math.BigInt => bi.toDouble
+    case e => throw new ParseException(s"Error converting item '$name' to Double. Value: $e, Type: ${e.getClass}",null)
   }
   
   def getString(name:String,in:Any):String = in match {
@@ -58,7 +64,7 @@ object PrimativeHelpers {
   def getDate(name:String, in:Any, dateFormat: DateFormat):Date = in match {
     case s:Date => s
     case s:String => dateFormat.parse(s).get
-    case e => throw new ParseException(s"Error converting item '$name' to Date. Value: $e",null)
+    case e => throw new ParseException(s"Error converting item '$name' to Date. Value: $e, Type: ${e.getClass}",null)
   }
   
   def optIdent[U](opt: Option[U]):Option[U] = opt
