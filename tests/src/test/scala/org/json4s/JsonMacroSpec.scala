95d9f540375ba2ce1f06d44518d6359ab09dac30
Bryce Anderson <bryce.anderson22@gmail.com>
2013-01-28 08:38:42 -0500

    Added support for BigInt and BigDecimal to macrohelpers
    Added unit tests that convert structures to and from Json using macros,
    value providers, and writers.

---
+++ tests/src/test/scala/org/json4s/JsonMacroSpec.scala
@@ -0,0 +1,40 @@
+package org.json4s
+
+import org.specs2.mutable.Specification
+import java.util.Date
+import Macros._
+
+import native.JsonMethods._  // For debugging purposes
+
+class JsonMacroSpec extends Specification {
+
+  // Needed to use the implicit calling style
+  implicit val dateFormats = DefaultFormats
+
+  case class Simple(one: Int, two: String)
+  case class Compound(one: Int, two: Simple)
+
+  "Macros with Json" should {
+    "Serialize and deserialize a simple case class" in {
+      val writer = JsonWriter.ast
+      val obj = Simple(1, "two")
+      serialize(obj,"simple",writer)
+
+      val valueProvider = new JsonValueProvider(writer.result)
+
+      deserialize[Simple](valueProvider,"simple") must_== obj
+
+    }
+
+    "Serialize and deserialize a compound case class" in {
+      val writer = JsonWriter.ast
+      val obj = Compound(1, Simple(1, "two"))
+      serialize(obj,"compound",writer)
+
+      val valueProvider = new JsonValueProvider(writer.result)
+
+      deserialize[Compound](valueProvider,"compound") must_== obj
+
+    }
+  }
+}
