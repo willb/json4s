78c7d9fa62838e2b015a7e93dc9c93937c7b6575
Bryce Anderson <bryce.anderson22@gmail.com>
2013-02-28 21:58:39 -0500

    Finished Deserializer rework. Still needs polish.

--- macros/src/main/scala/org/json4s/macroimpls/Deserializer.scala
+++ macros/src/main/scala/org/json4s/macroimpls/Deserializer.scala
@@ -70,46 +70,34 @@ object Deserializer {
       }
       }.tree
     } // rparseList
-    /*
-    def rparseMap(tpe:Type, name:c.Expr[String], params: c.Expr[JValue])   = {
+
+    def rparseMap(tpe:Type, params: c.Expr[JValue])   = {
       val TypeRef(_, _, keyTpe::valTpe::Nil) = tpe
-      val (freshNme, freshParams, freshParamsTree) = genFreshParams(name, params)
-      
       // Capable of parsing maps that contain primatives as keys, not only strings
       val kExpr = c.Expr[String](Ident("k"))
       val keyParser = keyTpe match {
-        case a if a =:= typeOf[Int] => reify{getInt(kExpr.splice, kExpr.splice)}
-        case a if a =:= typeOf[Long] => reify{getLong(kExpr.splice, kExpr.splice)}
-        case a if a =:= typeOf[Float] => reify{getFloat(kExpr.splice, kExpr.splice)}
-        case a if a =:= typeOf[Double] => reify{getDouble(kExpr.splice, kExpr.splice)}
-        case a if a =:= typeOf[String] => reify{kExpr.splice}
+        case a if a =:= typeOf[Int] => reify{getInt(JInt(kExpr.splice.toInt))}
+        case a if a =:= typeOf[Long] => reify{getLong(JInt(kExpr.splice.toLong))}
+        case a if a =:= typeOf[Float] => reify{getFloat(JDouble(kExpr.splice.toDouble))}
+        case a if a =:= typeOf[Double] => reify{getDouble(JDouble(kExpr.splice.toDouble))}
+        case a if a =:= typeOf[String] => reify{getString(JString(kExpr.splice))}
         case _ => c.abort(c.enclosingPosition, "Map must contain primative types as keys!")
       }
 
-      // Build the tree much like the function toMap does
-      val mapBuilderTree = ValDef(Modifiers(), newTermName("b"), TypeTree(),
-          TypeApply(reify{scala.collection.mutable.HashMap.empty}.tree,
-            List(typeArgumentTree(keyTpe), typeArgumentTree(valTpe))
-          )
-        )
-
-      val addValTree = Apply(Select(Ident("b"), newTermName("update")),
-                            List(keyParser.tree, buildObject(valTpe, kExpr, freshParams))
-                            )
-   
       reify {
-        // This is more simple to do using collections functions, but this is more 
-        // performant. Check older commits for simpler version 
-        // Jan 9, 2013 (da050fb6d1c317228cbcedeac9db91221d4565da)
-        c.Expr(freshParamsTree).splice
-        c.Expr(mapBuilderTree).splice // Defines val b = mapBuilder
-        freshParams.splice.keySet.foreach { k => // Must use k or rename above
-          c.Expr(addValTree).splice
+        params.splice match {
+          case JObject(objs) =>
+            objs.map { case (k, v) => (
+              keyParser.splice,
+              c.Expr(buildObject(valTpe, c.Expr[JValue](Ident("v")))).splice
+            )
+            }.toMap
+
+          case o => throw new JsonStructureException(JObject.getClass(), o.getClass())
         }
-        c.Expr(Select(Ident("b"), newTermName("toMap"))).splice
       }.tree
     }
-     */
+
     def rparseDate(params: c.Expr[JValue])  = reify {
       getDate(params.splice, defaultFormats.splice.dateFormat)
     }
@@ -140,13 +128,13 @@ object Deserializer {
         try{
           Some(c.Expr(buildObject(argTpe, params)).splice)
         } catch {
-          case _: NothingException => None
+          case _: JsonStructureException => None
         }
       }.tree
     }
 
     // The really heavyweight function. Most of the magic happens in the last else statement
-    def buildObject(tpe: Type, params: c.Expr[JValue]):Tree = {
+    def buildObject(tpe: Type, params: c.Expr[JValue]): Tree = {
       // simple types
       if      (tpe =:= typeOf[Int])    { rparseInt(params).tree    }
       else if (tpe =:= typeOf[Long])   { rparseLong( params).tree   }
@@ -158,23 +146,20 @@ object Deserializer {
       // The privileged types
       else if (tpe.erasure <:< typeOf[Option[Any]]) {
         rparseOption(tpe, params)
-      }       /*
+      }
       else if (tpe.erasure =:= typeOf[Map[_, _]]) {
-        rparseMap(tpe, name, params)
-      } */
+        rparseMap(tpe, params)
+      }
       else if (tpe.erasure =:= typeOf[List[Any]]) {
         rparseList(tpe, params)
       }
       else { // Must be a complex object
-      
         val TypeRef(_, sym:Symbol, tpeArgs:List[Type]) = tpe
         val ctorParams = tpe.member(nme.CONSTRUCTOR).asMethod.paramss
-
         
         val newObjTerm = newTermName(c.fresh("newObj$"))
         val newObjTypeTree = typeArgumentTree(tpe)
         val newObjTree = ValDef(Modifiers(), newObjTerm, newObjTypeTree,
-          // New(typeTree, List(List(params))
           New(newObjTypeTree, ctorParams.map{_.zipWithIndex.map {
             case (pSym, index) =>
             // Change out the types if it has type parameters
@@ -188,7 +173,7 @@ object Deserializer {
                 try {
                   c.Expr(buildObject(pTpe, freshParams)).splice // splice in another obj tree
                 } catch {
-                  case e: NothingException =>
+                  case e: JsonStructureException =>
                     // Need to use the origional symbol.companionObj to get defaults
                     // Would be better to find the generated TermNames if possible
                     c.Expr(Select(Ident(sym.companionSymbol), newTermName(
@@ -216,7 +201,7 @@ object Deserializer {
               )
             )).splice
             } catch { // Don't care if they fail
-              case _ : NothingException =>
+              case _ : JsonStructureException =>
             }
           }.tree)
         }
--- macros/src/main/scala/org/json4s/macroimpls/macrohelpers/PrimativeHelpers.scala
+++ macros/src/main/scala/org/json4s/macroimpls/macrohelpers/PrimativeHelpers.scala
@@ -6,7 +6,8 @@ import scala.reflect.macros.Context
 import org.json4s.ParserUtil.ParseException
 import org.json4s._
 
-class NothingException extends ParseException("JValue is a JNothing", null)
+class JsonStructureException(expected: Class[_], received: Class[_]) extends ParseException(
+  s"Json error: Type received ($received) but expected ($expected).", null)
 
 object PrimativeHelpers {
   import org.json4s.DateFormat
@@ -22,34 +23,34 @@ object PrimativeHelpers {
   // Some helpers to make things a little more simple in the generated code
   def getInt(in: JValue): Int = in match {
     case JInt(int) => int.intValue()
-    case _ => throw new NothingException
+    case _ => throw new JsonStructureException(JInt.getClass(), in.getClass())
   }
   
   def getLong(in: JValue): Long = in match {
     case JInt(int) => int.longValue()
-    case _ => throw new NothingException
+    case _ => throw new JsonStructureException(JInt.getClass(), in.getClass())
   }
   
   def getFloat(in: JValue): Float = in match {
     case JDouble(num)   => num.asInstanceOf[Float]
     case JDecimal(num)  => num.floatValue()
-    case _ => throw new NothingException
+    case _ => throw new JsonStructureException(JDecimal.getClass(), in.getClass())
   }
   
   def getDouble( in: JValue): Double = in match {
     case JDouble(num)   => num.asInstanceOf[Double]
     case JDecimal(num)  => num.doubleValue()
-    case _ => throw new NothingException
+    case _ => throw new JsonStructureException(JDecimal.getClass(), in.getClass())
   }
   
   def getString(in: JValue): String = in match {
     case JString(s) => s
-    case _ => throw new NothingException
+    case _ => throw new JsonStructureException(JString.getClass(), in.getClass())
   }
   
   def getDate(in: JValue, dateFormat: DateFormat): Date = in match {
     case JString(s) => dateFormat.parse(s).get
-    case _ => throw new NothingException
+    case _ => throw new JsonStructureException(JString.getClass(), in.getClass())
   }
 
   def optIdent[U](opt: Option[U]):Option[U] = opt
