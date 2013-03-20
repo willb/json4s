6b0c2c71c6152c670fd97fc1ff10ba79128548a0
Bryce Anderson <bryce.anderson22@gmail.com>
2013-03-19 20:28:25 -0400

    Refactored serializer
    Now doesn't use outer object. Cleaned up implementation quite a bit.

--- macros/src/main/scala/org/json4s/Macros.scala
+++ macros/src/main/scala/org/json4s/Macros.scala
@@ -5,9 +5,11 @@ import language.experimental.macros
 
 object Macros {
   
-  def serialize[U](obj: U, name: String, writer: JsonWriter[_])(implicit defaultFormats: Formats) =
-      macro macroimpls.Serializer.impl[U]
-  def serializeObj[U](obj: U)(implicit defaultFormats: Formats) = macro macroimpls.Serializer.implSerToObj[U]
+  def serialize[U](obj: U, writer: JsonWriter[_])(implicit defaultFormats: Formats) =
+      macro macroimpls.Serializer.serializeImpl[U]
+
+  def decompose[U](obj: U)(implicit defaultFormats: Formats) =
+      macro macroimpls.Serializer.decompose[U]
   
   def deserialize[U](params: JValue)(implicit defaultFormats: Formats) =
       macro macroimpls.Deserializer.deserialize_impl[U]
