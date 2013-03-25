12dfdf6cc70dc39957b9e64c84774a9b499274b1
Bryce Anderson <bryce.anderson22@gmail.com>
2013-03-24 20:01:18 -0400

    Kind of have list working
    Doesn't work with anything but list though.

--- macros/src/main/scala/org/json4s/macroimpls/Deserializer.scala
+++ macros/src/main/scala/org/json4s/macroimpls/Deserializer.scala
@@ -32,27 +32,10 @@ object Deserializer {
     val helpers = new MacroHelpers[c.type](c)
     import helpers._
 
+    def rparseDate(field: c.Expr[String], params: c.Expr[JsonObjectReader])  = reify {
+      defaultFormats.splice.dateFormat.parse(rparseString(field, params).splice).get
+    }
 
-//    def rparseList(tpe:Type, params: c.Expr[JsonArrayIterator]): Tree = {
-//      val TypeRef(_, _, List(argTpe)) = tpe
-//      val wrappedIndex: c.Expr[JValue] = c.Expr[JValue](Ident("i"))
-//
-//      reify{
-//        params.splice match {
-//          case JArray(arr) => arr.map { i =>
-//            c.Expr(buildObject(argTpe, wrappedIndex)).splice
-//          }
-//          case _ => Nil
-//      }
-//      }.tree
-//    } // rparseList
-//
-
-//
-//    def rparseDate(params: c.Expr[JValue])  = reify {
-//      getDate(params.splice, defaultFormats.splice.dateFormat)
-//    }
-//
     def rparseInt(field: c.Expr[String], params: c.Expr[JsonObjectReader])    = reify {
       params.splice.getInt(field.splice)
     }
@@ -72,21 +55,21 @@ object Deserializer {
     def rparseString(field: c.Expr[String], params: c.Expr[JsonObjectReader]) = reify {
       params.splice.getString(field.splice)
     }
-//
-//    def rparseSymbol(params: c.Expr[JValue]) = reify {
-//      Symbol(getString(params.splice))
-//    }
-//
-//    def rparseOption(tpe:Type, params: c.Expr[JsonReader]):Tree = {
-//      val TypeRef(_, _, List(argTpe)) = tpe
-//      reify{
-//        try{
-//          Some(c.Expr(buildObject(argTpe, params)).splice)
-//        } catch {
-//          case _: JsonStructureException => None
-//        }
-//      }.tree
-//    }
+
+    def rparseSymbol(field: c.Expr[String], params: c.Expr[JsonObjectReader]) = reify {
+      Symbol(rparseString(field, params).splice)
+    }
+
+    def rparseOption(tpe:Type, field: c.Expr[String], params: c.Expr[JsonObjectReader]):Tree = {
+      val TypeRef(_, _, List(argTpe)) = tpe
+      reify{
+        try{
+          Some(c.Expr(buildField(argTpe, field, params)).splice)
+        } catch {
+          case _: Throwable => None
+        }
+      }.tree
+    }
 
         def buildMap(tpe:Type, params: c.Expr[JsonObjectReader]): c.Tree   = {
     //      val TypeRef(_, _, keyTpe::valTpe::Nil) = tpe
@@ -116,107 +99,139 @@ object Deserializer {
           ???
         }
 
-    def buildList(tps: Type, reader: c.Expr[JsonArrayIterator]): Tree = {
-      ???
+    def buildList(tpe: Type, reader: c.Expr[JsonArrayIterator]): Tree = {
+      val TypeRef(_, _, List(argTpe)) = tpe
+      val builderExpr = Apply(Select(New(AppliedTypeTree(Ident("collection.mutable.ListBuffer"), List(Ident(argTpe.toString)))), nme.CONSTRUCTOR), List())
+
+
+//        reify{ // Cant seem to get this to work.
+//          val builder = c.Expr[scala.collection.mutable.ListBuffer[Any]](builderExpr).splice
+//          while(reader.splice.hasNext){
+//              builder.append(c.Expr[Any](buildCell(argTpe, reader)).splice)
+//          }
+//          builder.result()
+//        }.tree
+
+      val listNme = c.fresh("lst")
+      val listTree = ValDef(
+                        Modifiers(MUTABLE),
+                        listNme,
+                        typeArgumentTree(tpe),
+                        reify(Nil).tree
+            )
+
+      val itNme = c.fresh("jsonIterator")
+      val itExpr = c.Expr[JsonArrayIterator](Ident(itNme))
+      val itTree = ValDef(
+        Modifiers(),
+        newTermName(itNme),
+        TypeTree(typeOf[JsonArrayIterator]),
+        reader.tree
+      )
+
+        reify{
+          c.Expr(itTree).splice
+          c.Expr(listTree).splice
+          while(itExpr.splice.hasNext) {
+
+          // Manual tree manipulation is fastest, but more subject to scala churn
+            c.Expr(Assign(Ident(listNme),Apply(Select(Ident(listNme),
+                newTermName("$colon$colon")),
+                List(buildCell(argTpe, itExpr))))
+            ).splice
+
+          }
+          c.Expr[List[_]](Ident(listNme)).splice.reverse
+       }.tree
+    } // rparseList
+
+    def buildCell(pTpe: Type, reader: c.Expr[JsonArrayIterator]): Tree = {
+      if      (pTpe =:= typeOf[Int])         reify { reader.splice.nextInt }.tree
+      else if (pTpe =:= typeOf[Long])         reify { reader.splice.nextLong }.tree
+      else if (pTpe =:= typeOf[Float])         reify { reader.splice.nextFloat }.tree
+      else if (pTpe =:= typeOf[Double])         reify { reader.splice.nextDouble }.tree
+      else if (pTpe =:= typeOf[String])         reify { reader.splice.nextString }.tree
+      else if (typeOf[List[_]] <:< pTpe.erasure) buildList(pTpe, reify{reader.splice.nextArrayReader})
+      else buildObject(pTpe, reify{reader.splice.nextObjectReader})
+
+    }
+
+    def buildField(pTpe: Type, fieldName: c.Expr[String], params: c.Expr[JsonObjectReader]): Tree = {
+      if      (pTpe =:= typeOf[Int])         { rparseInt(fieldName, params).tree }
+      else if (pTpe =:= typeOf[Long])         { rparseLong(fieldName, params).tree  }
+      else if (pTpe =:= typeOf[Float])        { rparseFloat(fieldName, params).tree  }
+      else if (pTpe =:= typeOf[Double])       { rparseDouble(fieldName, params).tree }
+      else if (pTpe =:= typeOf[String])      { rparseString(fieldName, params).tree }
+      else if (pTpe =:= typeOf[Date])         { rparseDate(fieldName, params).tree   }
+      else if (pTpe =:= typeOf[scala.Symbol]) { rparseSymbol(fieldName, params).tree }
+      // The privileged types
+      else if (pTpe.erasure <:< typeOf[Option[_]]) {
+        rparseOption(pTpe, fieldName, params)
+      }
+      //      else if (typeOf[Map[_, _]] <:< tpe.erasure) {
+      //        rparseMap(tpe, params)
+      //      }
+      else if (typeOf[List[_]] <:< pTpe.erasure) {
+        buildList(pTpe, reify{params.splice.getArrayReader(fieldName.splice)})
+      }
+      else { // Must be complex
+        buildObject(pTpe, reify{ params.splice.getObjectReader(fieldName.splice)})
+      }
     }
 
     // The really heavyweight function. Most of the magic happens in the last else statement
     def buildObject(tpe: Type, params: c.Expr[JsonObjectReader]): Tree = {
-      // simple types
-//      if      (tpe =:= typeOf[Int])          { rparseInt(params).tree    }
-//      else if (tpe =:= typeOf[Long])         { rparseLong( params).tree  }
-//      else if (tpe =:= typeOf[Float])        { rparseFloat(params).tree  }
-//      else if (tpe =:= typeOf[Double])       { rparseDouble(params).tree }
-//      else if (tpe =:= typeOf[String])       { rparseString(params).tree }
-//      else if (tpe =:= typeOf[Date])         { rparseDate(params).tree   }
-//      else if (tpe =:= typeOf[scala.Symbol]) { rparseSymbol(params).tree }
-//
-//      // The privileged types
-//      else if (tpe.erasure <:< typeOf[Option[Any]]) {
-//        rparseOption(tpe, params)
-//      }
-//      else if (typeOf[Map[_, _]] <:< tpe.erasure) {
-//        rparseMap(tpe, params)
-//      }
-//      else if (typeOf[List[_]] <:< tpe.erasure) {
-//        rparseList(tpe, params)
-//      }
-//      else { // Must be a complex object
-        val TypeRef(_, sym: Symbol, tpeArgs: List[Type]) = tpe
-        val ctorParams = tpe.member(nme.CONSTRUCTOR).asMethod.paramss
-        
-        val newObjTerm = newTermName(c.fresh("newObj$"))
-        val newObjTypeTree = typeArgumentTree(tpe)
-        val newObjTree = ValDef(Modifiers(), newObjTerm, newObjTypeTree,
-          New(newObjTypeTree, ctorParams.map(_.zipWithIndex.map {
-            case (pSym, index) =>
-            // Change out the types if it has type parameters
-            val pTpe = pSym.typeSignature.substituteTypes(sym.asClass.typeParams, tpeArgs)
-            val fieldName = LIT(pSym.name.decoded)
-            //val (_, freshObj, freshObjTree) = getObjectField(fieldName, params)
 
-            //simple types
-            if      (pTpe =:= typeOf[Int])         { rparseInt(fieldName, params).tree }
-            else if (tpe =:= typeOf[Long])         { rparseLong(fieldName, params).tree  }
-            else if (tpe =:= typeOf[Float])        { rparseFloat(fieldName, params).tree  }
-            else if (tpe =:= typeOf[Double])       { rparseDouble(fieldName, params).tree }
-            else if (pTpe =:= typeOf[String])      { rparseString(fieldName, params).tree }
-//                  else if (tpe =:= typeOf[Date])         { rparseDate(params).tree   }
-//                  else if (tpe =:= typeOf[scala.Symbol]) { rparseSymbol(params).tree }
-            else { // Must be complex
-              buildObject(pTpe, reify{ params.splice.getObjectReader(fieldName.splice)})
-            }
+      val TypeRef(_, sym: Symbol, tpeArgs: List[Type]) = tpe
+      val ctorParams = tpe.member(nme.CONSTRUCTOR).asMethod.paramss
 
-            // If param has defaults, try to find the val in map, or call 
-            // default evaluation from its companion object
-//            Block(freshObjTree ,if (pSym.asTerm.isParamWithDefault) {
-//              reify {
-//                try {
-//                  c.Expr(buildObject(pTpe, freshObj)).splice // splice in another obj tree
-//                } catch {
-//                  case e: ParseException =>
-//                    // Need to use the origional symbol.companionObj to get defaults
-//                    // Would be better to find the generated TermNames if possible
-//                    c.Expr(Select(Ident(sym.companionSymbol), newTermName(
-//                      "$lessinit$greater$default$" + (index+1).toString))
-//                    ).splice
-//                }
-//              }.tree
-//            } else (reify { // repackage the exception to be more informative
-//              try c.Expr(buildObject(pTpe, freshObj)).splice
-//              catch {
-//                case e: ParseException =>
-//                  val msg = "Error getting field '" + fieldName.splice +
-//                             "' in object of type '" + LIT(tpe.toString).splice +
-//                             "'. Reason: " + e.toString
-//                  throw new ParseException(msg, e)
-//              }
-//            }.tree)) // Required
-          })) // Using the New(Tree, List(List(Tree))) constructor
-        ) // newObjTree ValDef
+      val newObjTerm = newTermName(c.fresh("newObj$"))
+      val newObjTypeTree = typeArgumentTree(tpe)
+      val newObjTree = ValDef(Modifiers(), newObjTerm, newObjTypeTree,
+        New(newObjTypeTree, ctorParams.map(_.zipWithIndex.map {
+          case (pSym, index) =>
+          // Change out the types if it has type parameters
+          val pTpe = pSym.typeSignature.substituteTypes(sym.asClass.typeParams, tpeArgs)
+          val fieldName = LIT(pSym.name.decoded)
+
+          // If param has defaults, try to find the val in map, or call
+          // default evaluation from its companion object
+          if (pSym.asTerm.isParamWithDefault) {
+            reify {
+              try {
+                c.Expr(buildField(pTpe, fieldName, params)).splice // splice in another obj tree
+              } catch {
+                case _: Throwable =>
+                  // Need to use the origional symbol.companionObj to get defaults
+                  // Would be better to find the generated TermNames if possible
+                  c.Expr(Select(Ident(sym.companionSymbol), newTermName(
+                    "$lessinit$greater$default$" + (index+1).toString))
+                  ).splice
+              }
+            }.tree
+          } else buildField(pTpe, fieldName, params)
+        })) // Using the New(Tree, List(List(Tree))) constructor
+      ) // newObjTree ValDef
         
-        // Generate the code for setting fields not in the constructor
-//        val vars = getNonConstructorVars(tpe)
-//        val setParamsBlocks = vars map { vari =>
-//          val varName = vari.name.toTermName.toString.trim
-//          val compName = LIT(varName)
-//          val (_, freshObj, freshObjTree) = getObjectField(compName, params)
-//          Block(freshObjTree, reify{
-//            try {
-//            c.Expr(Assign(Select(Ident(newObjTerm), newTermName(varName)),
-//              buildObject(
-//                vari.typeSignature,
-//                freshObj
-//              )
-//            )).splice
-//            } catch { // Don't care if they fail
-//              case _: JsonStructureException =>
-//            }
-//          }.tree)
-//        }
+      // Generate the code for setting fields not in the constructor
+      val setParamsBlocks = getNonConstructorVars(tpe).map{ pSym =>
+        val pTpe = pSym.typeSignature.substituteTypes(sym.asClass.typeParams, tpeArgs)
+        val varName = pSym.name.toTermName.toString.trim
+        val compName = LIT(varName)
+        reify{
+          try {
+          c.Expr(Assign(Select(Ident(newObjTerm), newTermName(varName)),
+          buildField(pTpe, compName, params)
+          )).splice
+          } catch { // Don't care if they fail
+            case _: JsonStructureException =>
+            case _: InvalidStructure =>
+          }
+        }.tree
+      }
         
         //Block(newObjTree::setParamsBlocks, Ident(newObjTerm))
-      Block(newObjTree::Nil, Ident(newObjTerm))
+      Block(newObjTree::setParamsBlocks, Ident(newObjTerm))
       //}
     }
     
@@ -247,7 +262,7 @@ object Deserializer {
         }
       }
     }
-    //println(expr)  // Debug
+    println(expr)  // Debug
     expr
   }
 }
