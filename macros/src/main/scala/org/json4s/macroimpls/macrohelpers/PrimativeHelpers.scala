19c738e0b571d9f145812b1ba2bb00fb1afbe5b6
Bryce Anderson <bryce.anderson22@gmail.com>
2013-03-25 20:35:56 -0400

    more cleanup
    Brought naming inline to be more consistent throughout the deserializer.

--- macros/src/main/scala/org/json4s/macroimpls/macrohelpers/PrimativeHelpers.scala
+++ macros/src/main/scala/org/json4s/macroimpls/macrohelpers/PrimativeHelpers.scala
@@ -1,9 +1,5 @@
 package org.json4s.macroimpls.macrohelpers
 
-import org.json4s.ParserUtil.ParseException
-
-//class JsonStructureException(expected: Class[_], received: Class[_]) extends ParseException(
-//  s"Received JValue type ($received) but expected ($expected).", null)
 
 object PrimativeHelpers {
   def optIdent[U](opt: Option[U]):Option[U] = opt
