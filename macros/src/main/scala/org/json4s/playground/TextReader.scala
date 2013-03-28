3bab92a5fb64f85b7243d3fbd4bb4aded306269f
Bryce Anderson <bryce.anderson22@gmail.com>
2013-03-28 18:50:59 -0400

    Moved to linear parsing
    Got a small performance boost too.

--- macros/src/main/scala/org/json4s/playground/TextReader.scala
+++ macros/src/main/scala/org/json4s/playground/TextReader.scala
@@ -3,6 +3,7 @@ package playground
 
 import java.lang.Character
 import annotation.tailrec
+import collection.mutable.ListBuffer
 
 /**
  * @author Bryce Anderson
@@ -11,103 +12,91 @@ import annotation.tailrec
 
 
 object TextReader {
-  def bindText(str: String) = {
-//    val cursor = new JsonTextCursor(str)
-//    cursor.trim()
-//    cursor.extractField() match {
-//      case cursor.JsonObject(str) => new TextObjectReader(str)
-//      case cursor.JsonArray(str)  => new TextArrayIterator(str)
-//      case e => cursor.fail(s"Invalid starting json structure: $e")
-//    }
-
-    // This way is significantly faster than zooming throughout the whole text as commented out above.
-    var beginning = 0
-    var end = str.length - 1
-    while(beginning < str.length && str.charAt(beginning) != '{' && str.charAt(beginning) != '[' ) beginning += 1
-    if (beginning == str.length) throw new java.lang.IllegalStateException(s"string doesn't contain json array or objects.")
-    if (str.charAt(beginning) == '{') {
-      while(end >= beginning && str.charAt(end) != '}') end -= 1
-      new TextObjectReader(str.substring(beginning+1, end))
-    } else{
-      while(end >= beginning && str.charAt(end) != ']') end -= 1
-      new TextArrayIterator(str.substring(beginning+1, end))
+  def bindText(str: String): JsonReader = {
+    val cursor = new JsonTextCursor(str)
+    cursor.trim()
+    cursor.extractField() match {
+      case JsonObject(obj) => obj
+      case JsonArray(obj)  => obj
+      case e => fail(s"Invalid starting json structure: $e")
     }
   }
+
+  private[json4s] def fail(msg: String) = throw new java.lang.IllegalStateException(msg)
+}
+
+private[json4s] sealed trait JsonField
+private[json4s] case object END extends JsonField
+private[json4s] case object Null extends JsonField
+private[json4s] case class JsonBool(v: Boolean) extends JsonField
+private[json4s] case class JsonString(str: String) extends JsonField
+private[json4s] case class JsonObject(reader: TextObjectReader) extends JsonField
+private[json4s] case class JsonArray(reader: TextArrayIterator) extends JsonField
+private[json4s] case class JsonNumber(str: String) extends JsonField {
+  def toInt = str.toInt
+  def toLong = str.toLong
+  def toBigInt = BigInt(str)
+  def toDouble = str.toDouble
+  def toFloat = str.toFloat
+  def toBigDecimal = BigDecimal(str)
 }
 
 
-private[json4s] class JsonTextCursor(txt: String) {
-
-  private[json4s] sealed trait JsonField
-  private[json4s] case object END extends JsonField
-  private[json4s] case object Null extends JsonField
-  private[json4s] case class JsonBool(v: Boolean) extends JsonField
-  private[json4s] case class JsonString(str: String) extends JsonField
-  private[json4s] case class JsonObject(str: String) extends JsonField
-  private[json4s] case class JsonArray(str: String) extends JsonField
-  private[json4s] case class JsonNumber(str: String) extends JsonField {
-    def toInt = str.toInt
-    def toLong = str.toLong
-    def toBigInt = BigInt(str)
-    def toDouble = str.toDouble
-    def toFloat = str.toFloat
-    def toBigDecimal = BigDecimal(str)
-  }
+private[json4s] class JsonTextCursor(txt: String) { self =>
 
   private var current = 0
   private val maxLength = txt.length
 
-  def empty = (current == maxLength)
+  def empty = (current >= maxLength)
 
   def remainder = txt.substring(current)
+
+  final def nextChar() = { current += 1; txt.charAt(current-1)}
 
   def isDelimiter(c: Char) = c == ' ' || c == '\n' || c == ',' || c == '\r' || c == '\t' || c == '}' || c == ']'
   def isNumberChar(c: Char) = (Character.isDigit(c) || c == '.' || c == 'e' || c == 'E' || c == '-' || c == '+')
   def isWhitespace(in: Char) = ( in == ' ' || in == '\r' || in == '\t' || in == '\n')
 
-  def fail(msg: String) = throw new java.lang.IllegalStateException(msg)
+  def findNextString(): JsonString = {
+    if(txt.charAt(current) != '"') TextReader.fail(s"Failed to find string next in '$remainder'")
+    var begin = current + 1
+    var end = begin
 
-
-  def unescapeString(txt: String): String = {
     var builder: StringBuilder = null
-    var begin = 0
-    var end = 0
-    while(end < txt.length) {
+    var chr = txt.charAt(end)
+    while(chr != '"') {
       if(txt.charAt(end) == '\\') {
-        if (builder == null) builder = new StringBuilder(txt.length) // Lazy is good
+        if (builder == null) builder = new StringBuilder(txt.length)
 
         builder.append(txt.substring(begin, end))
         end +=1
-        val c = txt.charAt(end)
-        if(c == '"' || c == '\\' || c == '/') { builder.append(c) }
-        else if (c == 'b') { builder.append("\b") }
-        else if (c == 'f') { builder.append("\f") }
-        else if (c == 'n') { builder.append("\n") }
-        else if (c == 'r') { builder.append("\r") }
-        else if (c == 't') { builder.append("\t") }
-        else if (c == 'u' ) { builder.append(Integer.parseInt(txt.substring(end+1, end+5), 16).toChar); end +=5 }
-
+        txt.charAt(end) match {
+          case c if(c == '"' || c == '\\' || c == '/') => builder.append(c)
+          case c if (c == 'b') => builder.append("\b")
+          case c if (c == 'f') => builder.append("\f")
+          case c if (c == 'n') => builder.append("\n")
+          case c if (c == 'r') => builder.append("\r")
+          case c if (c == 't') => builder.append("\t")
+          case c if (c == 'u') => { builder.append(Integer.parseInt(txt.substring(end+1, end+5), 16).toChar); end +=4 }
+          case c => TextReader.fail(s"Bad escaped character: '$c'. Remainder: ${remainder}")
+        }
         begin = end + 1
       }
 
       end += 1
+      require(end < txt.length)
+      chr = txt.charAt(end)
     }
 
-    if (begin == 0) txt
+    if (begin == current + 1) {
+      current = end + 1
+      JsonString(txt.substring(begin, end))
+    }
     else {
-      if (begin < txt.length) builder.append(txt.substring(begin, txt.length))
-      builder.result()
+      current = end + 1
+      if (begin < end) builder.append(txt.substring(begin, end))
+      JsonString(builder.result())
     }
-  }
-
-  def findNextString(): String = {
-    if(txt.charAt(current) != '"') fail(s"Failed to find string next in '$remainder'")
-    var end = current + 1
-    while(!(txt.charAt(end) == '"' && checkEscaped(end))) end +=1
-
-    val str = unescapeString(txt.substring(current + 1, end))
-    current = end + 1
-    str
   }
 
   @inline
@@ -122,34 +111,35 @@ private[json4s] class JsonTextCursor(txt: String) {
   @inline final def trim() { while(current < maxLength && isWhitespace(txt.charAt(current))) current += 1 }
 
   // Requires a separator
-  def zoomPastSeparator(sep: Char, required: Boolean = false) {
+  // returns true if finished, otherwise false
+  def zoomPastSeparator(sep: Char, end: Char): Boolean = {
     trim()
-    if (empty) {
-      if(required) fail(s"Didn't find separator symbol '$sep'")
-    }
+    val chr = txt.charAt(current)
+    if (chr == end) true
+    else if (chr != sep) TextReader.fail(s"Separator '${txt.charAt(current)}' is not '$sep' or '$end'")
     else {
-      if (txt.charAt(current) != sep) fail(s"Separator '${txt.charAt(current)}' is not '$sep'")
       current += 1
       trim()
+      false
     }
   }
 
-  def findNextNumber(): String = {
+  def findNextNumber(): JsonNumber = {
     var end = current
     while(end < maxLength && isNumberChar(txt.charAt(end))) end += 1
 
     val str = txt.substring(current, end)
     current = end
-    str
+    JsonNumber(str)
   }
 
-  def findNextBoolean(): Boolean = {
+  def findNextBoolean(): JsonBool = {
     if (txt.charAt(current)     == 't' &&
         txt.charAt(current + 1) == 'r' &&
         txt.charAt(current + 2) == 'u' &&
         txt.charAt(current + 3) == 'e') {
       current = current + 4
-      true
+      JsonBool(true)
     }
     else if ( txt.charAt(current)     == 'f' &&
               txt.charAt(current + 1) == 'a' &&
@@ -157,156 +147,149 @@ private[json4s] class JsonTextCursor(txt: String) {
               txt.charAt(current + 3) == 's' &&
               txt.charAt(current + 4) == 'e') {
       current = current + 5
-      false
+      JsonBool(false)
     }
-    else fail(s"Next token is not of type boolean: ${txt.substring(current)}")
-  }
-
-  def findNextObject() = findNextBlock('{', '}')
-  def findNextArray() = findNextBlock('[', ']')
-
-  private def findNextBlock(start: Char, finish: Char): String = {
-    var c = ' '
-    while({c = txt.charAt(current); c != start}) {
-      if (c == '"') {
-        current +=1
-        while(!(txt.charAt(current) == '"' &&  checkEscaped(current))) current +=1
-      }
-      current += 1
-    }
-
-    var end = current + 1
-    var innerObjs = 0
-    while({c = txt.charAt(end); c != finish || innerObjs > 0}) {
-      if (c == '"') {
-        end +=1
-        while(!(txt.charAt(end) == '"' &&  txt.charAt(end-1) != '\\')) end +=1
-      }
-      else if (c == start) innerObjs += 1
-      else if (c == finish) innerObjs -= 1
-      end +=1
-    }
-    val str = txt.substring(current+1, end)
-    current = end + 1
-    str
+    else TextReader.fail(s"Next token is not of type boolean: ${txt.substring(current)}")
   }
 
   def extractField(): JsonField =
     if(maxLength == current) {
-      fail(s"Tried to extract field that doesn't exist!")
+      TextReader.fail(s"Tried to extract field that doesn't exist!")
     } else txt.charAt(current) match {
-    case '"'                    => JsonString(findNextString)
-    case '{'                    => JsonObject(findNextObject)
-    case '['                    => JsonArray(findNextArray)
-    case c if (isNumberChar(c)) => JsonNumber(findNextNumber)
-    case c if(c == 't' || c == 'f') => JsonBool(findNextBoolean)
+    case '"'                    => findNextString()
+    case '{'                    => JsonObject(new TextObjectReader(self))
+    case '['                    => JsonArray(new TextArrayIterator(self))
+    case c if (isNumberChar(c)) => findNextNumber()
+    case c if(c == 't' || c == 'f') => findNextBoolean()
     case 'n' if (txt.charAt(current + 1) == 'u' && txt.charAt(current + 2) == 'l' && txt.charAt(current + 3) == 'l') =>
        current += 4
        Null
 
-    case _ => fail(s"Failed to extract field from remaining json: ${txt.substring(current)}")
+    case _ => TextReader.fail(s"Failed to extract field from remaining json: ${txt.substring(current)}")
   }
-
 }
 
-final class TextObjectReader(txt: String) extends JsonTextCursor(txt) with JsonObjectReader {
+final class TextObjectReader(cursor: JsonTextCursor) extends JsonObjectReader {
 
   // txt should not include the brackets {}, but everything just inside them
-  //def isSeparator(in: Char) = ( in == ' ' || in == '\r' || in == '\t' || in == '\n' || in == ',' || in == ':')
   val fields: List[(String, JsonField)] = {
+    if(cursor.nextChar() != '{' ) TextReader.fail(s"Json is not an object: ${cursor.remainder}")
+    cursor.trim()
+
     val builder = new collection.mutable.ListBuffer[(String, JsonField)]()
 
     @tailrec
     def looper(): Unit = {
-      val key = findNextString()
-      zoomPastSeparator(':', true)
-      val value = extractField()
+      val JsonString(key) = cursor.findNextString()
+      if (cursor.zoomPastSeparator(':', '}')) TextReader.fail(s"Invalid json. Remainder: '${cursor.remainder}'")
+      val value = cursor.extractField()
       builder += ((key, value))
-      zoomPastSeparator(',')
-      if (!empty) looper()
+      if(!cursor.zoomPastSeparator(',', '}')) {
+        looper()
+      }
     }
-
-    trim()
-    if (!empty) looper()
+    looper()
+    if(cursor.nextChar() != '}' ) TextReader.fail(s"Json object missing closing bracket.: ${cursor.remainder}")
     builder.result
   }
 
-  def getField(name: String) = fields.find(_._1 == name).map(_._2)
+  def getField(name: String) = {
+    @tailrec def looper(lst: List[(String, JsonField)]): Option[JsonField] = lst match {
+      case Nil => None
+      case l if (l.head._1 == name) => Some(l.head._2)
+      case l => looper(l.tail)
+    }
+    looper(fields)
+  }
 
   lazy val getKeys: Seq[String] = fields.map(_._1)
 
   // Option forms
   def optObjectReader(key: String): Option[JsonObjectReader] = getField(key).map ( _ match {
-    case JsonObject(str) => new TextObjectReader(str)
-    case e => fail(s"Field '$key' doesn't contain object: ${e.toString}")
+    case JsonObject(reader) => reader
+    case e => TextReader.fail(s"Field '$key' doesn't contain object: ${e.toString}")
   })
 
   def optArrayReader(key: String): Option[JsonArrayIterator] = getField(key).map ( _ match {
-    case JsonArray(str) => new TextArrayIterator(str)
-    case e => fail(s"Field '$key' doesn't contain array: ${e.toString}")
+    case JsonArray(reader) => reader
+    case e => TextReader.fail(s"Field '$key' doesn't contain array: ${e.toString}")
   })
 
   def optInt(key: String): Option[Int] = getField(key).map ( _ match {
     case n: JsonNumber => n.toInt
-    case e => fail(s"Field '$key' doesn't contain number: ${e.toString}")
+    case e => TextReader.fail(s"Field '$key' doesn't contain number: ${e.toString}")
   })
 
   def optLong(key: String): Option[Long] = getField(key).map ( _ match {
     case n: JsonNumber => n.toLong
-    case e => fail(s"Field '$key' doesn't contain number: ${e.toString}")
+    case e => TextReader.fail(s"Field '$key' doesn't contain number: ${e.toString}")
   })
 
   def optFloat(key: String): Option[Float] = getField(key).map ( _ match {
     case n: JsonNumber => n.toFloat
-    case e => fail(s"Field '$key' doesn't contain number: ${e.toString}")
+    case e => TextReader.fail(s"Field '$key' doesn't contain number: ${e.toString}")
   })
 
   def optDouble(key: String): Option[Double] = getField(key).map ( _ match {
     case n: JsonNumber => n.toDouble
-    case e => fail(s"Field '$key' doesn't contain number: ${e.toString}")
+    case e => TextReader.fail(s"Field '$key' doesn't contain number: ${e.toString}")
   })
 
   def optBigInt(key: String): Option[BigInt] = getField(key).map ( _ match {
     case n: JsonNumber => n.toBigInt
-    case e => fail(s"Field '$key' doesn't contain number: ${e.toString}")
+    case e => TextReader.fail(s"Field '$key' doesn't contain number: ${e.toString}")
   })
 
   def optBigDecimal(key: String): Option[BigDecimal] = getField(key).map ( _ match {
     case n: JsonNumber => n.toBigDecimal
-    case e => fail(s"Field '$key' doesn't contain number: ${e.toString}")
+    case e => TextReader.fail(s"Field '$key' doesn't contain number: ${e.toString}")
   })
 
   def optBool(key: String): Option[Boolean] = getField(key).map ( _ match {
     case JsonBool(v) => v
-    case e => fail(s"Field '$key' doesn't contain Boolean: ${e.toString}")
+    case e => TextReader.fail(s"Field '$key' doesn't contain Boolean: ${e.toString}")
   })
 
   def optString(key: String): Option[String] = getField(key).map ( _ match {
     case JsonString(str) => str
-    case e => fail(s"Field '$key' doesn't contain number: ${e.toString}")
+    case e => TextReader.fail(s"Field '$key' doesn't contain number: ${e.toString}")
   })
 }
 
-final class TextArrayIterator(txt: String) extends JsonTextCursor(txt) with JsonArrayIterator {
+final class TextArrayIterator(cursor: JsonTextCursor) extends JsonArrayIterator {
 
-  trim()
+  var fields: List[JsonField] = {
+    if(cursor.nextChar() != '[' ) TextReader.fail(s"Json is not an array: ${cursor.remainder}")
+    cursor.trim()
+    val builder = new ListBuffer[JsonField]
+    @tailrec
+    def looper(): Unit = {
+      builder += cursor.extractField()
+      if(!cursor.zoomPastSeparator(',', ']')) looper()
+    }
+
+    looper()
+    if(cursor.nextChar() != ']' ) TextReader.fail(s"Json object missing closing bracket!: ${cursor.remainder}")
+    builder.result()
+  }
 
   private def nextField: JsonField = {
-    val value = extractField()
-    zoomPastSeparator(',', false)
+    if(fields.isEmpty) TextReader.fail(s"TextArrayIterator is empty!")
+    val value = fields.head
+    fields = fields.tail
     value
   }
 
-  def hasNext: Boolean = !empty
+  def hasNext: Boolean = !fields.isEmpty
 
   // Option forms
   def getNextObjectReader: Option[JsonObjectReader] = nextField match {
-    case JsonObject(str) => Some(new TextObjectReader(str))
+    case JsonObject(obj) => Some(obj)
     case _ => None
   }
 
   def getNextArrayReader: Option[JsonArrayIterator] = nextField match {
-    case JsonArray(str) => Some(new TextArrayIterator(str))
+    case JsonArray(arr) => Some(arr)
     case _ => None
   }
 
