618ecaedd22892ae2e2053bc09bd3029b707178d
Ivan Porto Carrero <ivan@flanders.co.nz>
2013-03-31 22:11:55 +0200

    provide Reader and Writer type classes based on macros

--- core/src/main/scala/org/json4s/Macros.scala
+++ core/src/main/scala/org/json4s/Macros.scala
@@ -24,41 +24,25 @@ object Macros extends Macros
 
 trait MacroReader[U] extends Reader[U] {
   def extract(jvalue: JValue)(implicit defaultFormats: Formats): U =
-    Macros.deserialize[U](macro_readers.AstReader(jvalue))
+    macro macroimpls.Deserializer.extract_impl[U]
 
-  def read(str: String)(implicit defaultFormats: Formats): U = Macros.read(str)
+  def read(str: String)(implicit defaultFormats: Formats): U =
+  macro macroimpls.Deserializer.read_impl[U]
 }
 
 trait MacroWriter[-U] extends Writer[U] {
-  def write(obj: U)(implicit defaultFormats: Formats): JValue = Macros.decompose[U](obj)
-  def writeToStream(obj: U, writer: java.io.Writer)(implicit defaultFormats: Formats): java.io.Writer = {
-    val w = JsonWriter.streaming(writer)
-    Macros.serialize(obj, w)
-    writer
-  }
-  def writeToStreamPretty(obj: U, writer: java.io.Writer)(implicit defaultFormats: Formats): java.io.Writer = {
-    val w = JsonWriter.streamingPretty(writer)
-    Macros.serialize(obj, w)
-    writer
-  }
-  def writeString(obj: U)(implicit defaultFormats: Formats): String = {
-    val sw = new SegmentedStringWriter(new BufferRecycler)
-    try {
-      Macros.serialize(obj, JsonWriter.streaming(sw))
-      sw.getAndClear
-    } finally {
-      sw.close()
-    }
-  }
-  def writeStringPretty(obj: U)(implicit defaultFormats: Formats): String = {
-    val sw = new SegmentedStringWriter(new BufferRecycler)
-    try {
-      Macros.serialize(obj, JsonWriter.streamingPretty(sw))
-      sw.getAndClear
-    } finally {
-      sw.close()
-    }
-  }
+  def write(obj: U)(implicit defaultFormats: Formats): JValue =
+    macro macroimpls.Serializer.decompose[U]
+  def writeToStream[W <: java.io.Writer](obj: U, w: W)(implicit defaultFormats: Formats): W =
+    macro macroimpls.Serializer.serializeToStreamWriter[U, W]
+
+  def writeToStreamPretty[W <: java.io.Writer](obj: U, w: W)(implicit defaultFormats: Formats): W =
+    macro macroimpls.Serializer.serializePrettyToStreamWriter[U, W]
+
+  def writeString(obj: U)(implicit defaultFormats: Formats): String =
+    macro macroimpls.Serializer.serializeToString[U]
+
+  def writeStringPretty(obj: U)(implicit defaultFormats: Formats): String =
+    macro macroimpls.Serializer.serializePrettyToString[U]
 }
 
-trait MacroJsonFormat[U] extends MacroReader[U] with MacroWriter[U] with JsonFormat[U]
